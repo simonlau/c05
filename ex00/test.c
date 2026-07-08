4497e65b066503aeb6d619690f91c0cd0b2a4ad7
Simon Lau <simon.lau.sg@gmail.com>
2026-07-08 22:16:46 +0800

    feat: did ex00

---
+++ ex00/test.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+int		ft_iterative_factorial(int nb);
+
+void	test_invalid(void)
+{
+	assert(0 == ft_iterative_factorial(0));
+	assert(0 == ft_iterative_factorial(-1));
+	assert(0 == ft_iterative_factorial(-9));
+}
+
+void	test_valid(void)
+{
+	assert(1 == ft_iterative_factorial(1));
+	assert(362880 == ft_iterative_factorial(9));
+}
+int	main(void)
+{
+	test_invalid();
+	test_valid();
+}

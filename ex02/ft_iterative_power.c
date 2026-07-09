1c3efb2116e6198366dfe5a2fc2cd6ca018db9e0
Simon Lau <simon.lau.sg@gmail.com>
2026-07-09 13:03:46 +0800

    style: fix header

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -1,4 +1,16 @@
-int	ft_iterative_power(int nb, int power)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simon.lau <simon.lau@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 13:02:29 by simon.lau         #+#    #+#             */
+/*   Updated: 2026/07/09 13:02:29 by simon.lau        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
 {
 	int	result;
 	int	i;
--- ex02/test.c
+++ ex02/test.c
@@ -1,23 +1,23 @@
 #include <assert.h>
 
-int		ft_iterative_power(int nb, int power);
+int		ft_recursive_power(int nb, int power);
 
 void	test_invalid(void)
 {
-	assert(0 == ft_iterative_power(42, -1));
-	assert(0 == ft_iterative_power(42, -42));
+	assert(0 == ft_recursive_power(42, -1));
+	assert(0 == ft_recursive_power(42, -42));
 }
 
 void	test_valid(void)
 {
-	assert(8 == ft_iterative_power(2, 3));
-	assert(16 == ft_iterative_power(2, 4));
-	assert(3111696 == ft_iterative_power(42, 4));
+	assert(8 == ft_recursive_power(2, 3));
+	assert(16 == ft_recursive_power(2, 4));
+	assert(3111696 == ft_recursive_power(42, 4));
 }
 
 void	test_zero(void)
 {
-	assert(0 == ft_iterative_power(0, 0));
+	assert(0 == ft_recursive_power(0, 0));
 }
 
 int	main(void)
